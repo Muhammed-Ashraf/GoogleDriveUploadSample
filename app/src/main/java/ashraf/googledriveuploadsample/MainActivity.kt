f95586197aa3150ac1ef64e4340cbf402a9b2843
ashraf probook <ashraf@itware.ae>
2023-05-30 14:43:57 +0400

    Add view Binding

--- app/src/main/java/ashraf/googledriveuploadsample/MainActivity.kt
+++ app/src/main/java/ashraf/googledriveuploadsample/MainActivity.kt
@@ -2,6 +2,7 @@ package ashraf.googledriveuploadsample
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import ashraf.googledriveuploadsample.databinding.MainLayoutBinding
 import ashraf.googledriveuploadsample.ui.theme.GoogleDriveUploadSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,24 +27,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AndroidView(
-                        factory = { context ->
-                            val view = LayoutInflater.from(context).inflate(R.layout.main_layout, null, false)
-//                            val textView = view.findViewById<TextView>(R.id.text)
-
-                            // do whatever you want...
-                            view // return the view
-                        },
-                        update = { view ->
-                            // Update the view
-                        }
-                    )
-
-                    Greeting("Android")
+                    // XmlView("Hello")
+                    AndroidViewBindingExample()
                 }
             }
         }
     }
+}
+
+@Composable
+fun XmlView(text: String) {
+    AndroidView(
+        factory = { context ->
+            val view = LayoutInflater.from(context).inflate(R.layout.main_layout, null, false)
+
+            // do whatever you want...
+            view // return the view
+        },
+        update = { view ->
+
+            val textView = view.findViewById<TextView>(R.id.mTextView)
+            textView.setText(text)
+        }
+    )
 }
 
 @Composable
@@ -51,8 +59,8 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GoogleDriveUploadSampleTheme {
-        Greeting("Android")
+fun AndroidViewBindingExample() {
+    AndroidViewBinding(MainLayoutBinding::inflate) {
+        mTextView.text = "How are you??"
     }
 }
